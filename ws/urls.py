470ca26acf03171770d4541fc93a8edfe5b2539c
jopro-git <63972157+jopro-git@users.noreply.github.com>
2024-08-28 20:31:22 +0000

    add about

--- ws/urls.py
+++ ws/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='shop-home'),
+    path('about', views.about, name ='shop-about')
 ]
--- ws/views.py
+++ ws/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'ws/index.html')
+
+def about(request):
+    return render(request, 'ws/about.html')
